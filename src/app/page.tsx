a75bddd49a872185f9a2acaf8a999ee0a688629b
BenBlakesley <benblakesley9@gmail.com>
2025-03-16 20:51:04 +0000

    get rid of fight route and do everything on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,46 @@
-import { Box, Button } from "@mui/material";
-import Link from "next/link";
+'use client';
 
-export default function Home() {
-  return (
-    <Box>
-      hi
-      <Link href="/fight">
-        <Button>
-          FIGHT
-        </Button>
-      </Link>
-    </Box>
-  );
+import { PokeService } from "@/api/PokeService";
+import { useAppDispatch, useAppSelector } from "@/state/hooks";
+import { setCurrentPokemon, setCurrentScore, setGameInProgress } from "@/state/reducers/progressSlice";
+import { Box } from "@mui/material";
+import { useEffect, useRef } from "react";
+import { FightArena } from "../fight/FightArena";
+
+export default function FightPage()
+{
+    const dispatch = useAppDispatch();
+    const progressState = useAppSelector(state => state.progress)
+    const {currentPokemon} = progressState;
+    const pokeA = currentPokemon?.pokemonA;
+    const pokeB = currentPokemon?.pokemonB;
+
+    const pokeService = PokeService.getInstance();
+
+    const count = useRef(0);
+
+    useEffect(() => 
+    {
+        const setInitFightData = async () =>
+        {
+            const [pokeA, pokeB] = await Promise.all([
+                pokeService.getRandomPokemon(),
+                pokeService.getRandomPokemon()
+            ]);
+
+            dispatch(setCurrentPokemon({pokemonA: pokeA, pokemonB: pokeB}));
+            dispatch(setGameInProgress(true));
+            dispatch(setCurrentScore(0));
+        }
+
+
+        count.current++
+        setInitFightData();
+    }, []);
+
+    return(
+        <Box>
+            {!!pokeA && !!pokeB && <FightArena pokemonA={pokeA} pokemonB={pokeB}/>}
+        </Box>
+    )
 }
