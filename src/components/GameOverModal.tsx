4418bd258f7c7064087aa026571e9c4ff3cb00c6
BenBlakesley <benblakesley9@gmail.com>
2025-03-17 09:48:33 +0000

    background on game over modal

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -10,6 +10,7 @@ export function GameOverModal()
     const dispatch = useAppDispatch();
 
     const {open} = useAppSelector(state => state.modal);
+    const {lastGameTotalScore} = useAppSelector(state => state.progress)
 
     return (
             <Modal open={open} onClose={() => dispatch(closeModal())}>
@@ -19,21 +20,29 @@ export function GameOverModal()
                         top: "50%",
                         left: "50%",
                         transform: "translate(-50%, -50%)",
-                        width: 400,
-                        bgcolor: "background.paper",
+                        width: 300,
+                        height: 400,
                         boxShadow: 24,
                         p: 4,
                         borderRadius: 2,
                         display: "flex",
                         flexDirection: "column",
-                        alignItems: "center"
+                        alignItems: "center",
+                        backgroundImage: 'url(/pikachu.avif)',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center center',
+                        backgroundRepeat: 'no-repeat',
                     }}
                     >
-                    <Typography>
-                        Game Over
+                    <Typography sx = {{fontSize: "2.5em", fontWeight: 800}}>
+                        Game Over!
+                    </Typography>
+                    <Typography sx = {{fontSize: "1.5em", fontWeight: 600}}>
+                        You scored {lastGameTotalScore}
                     </Typography>
                     <Box>
-                        <Button onClick={() => dispatch(closeModal())}>
+                        <Button onClick={() => dispatch(closeModal())}
+                            sx={{fontSize: "1.3em", fontWeight: 600}}>
                             Play Again
                         </Button>
                     </Box>
