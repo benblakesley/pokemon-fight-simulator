170373187d7f6f66fea964cd42fa359957de3940
BenBlakesley <benblakesley9@gmail.com>
2025-03-17 09:54:11 +0000

    add back setInitFightDate for when current score is 0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,39 @@
 'use client';
 
-import { useAppSelector } from "@/state/hooks";
+import { PokeService } from "@/api/PokeService";
+import { useAppDispatch, useAppSelector } from "@/state/hooks";
+import { setCurrentPokemon } from "@/state/reducers/progressSlice";
 import { Box } from "@mui/material";
+import { useEffect } from "react";
 import { FightArena } from "../fight/FightArena";
 
 export default function FightPage()
 {
+    const dispatch = useAppDispatch();
     const progressState = useAppSelector(state => state.progress)
-    const {currentPokemon} = progressState;
+    const {currentPokemon, currentScore} = progressState;
     const pokeA = currentPokemon?.pokemonA;
     const pokeB = currentPokemon?.pokemonB;
+
+    const pokeService = PokeService.getInstance();
+
+    useEffect(() => 
+    {
+        const setInitFightData = async () =>
+        {
+            const [pokeA, pokeB] = await Promise.all([
+                pokeService.getRandomPokemon(),
+                pokeService.getRandomPokemon()
+            ]);
+
+            dispatch(setCurrentPokemon({pokemonA: pokeA, pokemonB: pokeB}));
+        }
+
+        if(currentScore === 0)
+        {
+            setInitFightData();
+        }
+    }, []);
 
     return(
         <Box>
