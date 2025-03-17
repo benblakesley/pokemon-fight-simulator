652d5c115631916ca0eda2dd47bd5fb5457459a3
BenBlakesley <benblakesley9@gmail.com>
2025-03-17 16:44:17 +0000

    temporary background for redux persistor loading state

--- src/state/ClientPersistGate.tsx
+++ src/state/ClientPersistGate.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Box } from "@mui/material";
 import { persistor } from "./store";
 import { ReactNode } from "react";
 import { PersistGate } from "redux-persist/integration/react";
@@ -12,7 +13,21 @@ interface ClientProviderProps {
 export function ClientPersistGate({ children }: ClientProviderProps)
 {      
   return (
-    <PersistGate loading={null} persistor={persistor}>
+    <PersistGate 
+    loading={<Box sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    backgroundImage: 'url(/fightArena.jpg)',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                    backgroundRepeat: 'no-repeat',
+                    height: '100vh',
+                    width: '100%',
+                  }}
+              />}
+      persistor={persistor}>
       {children}
     </PersistGate>
   );
